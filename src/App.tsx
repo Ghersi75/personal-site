375f4c4cf86ba42505b2338a8447c3ca11cd0d9b
Ghersi Mac <34931324+Ghersi75@users.noreply.github.com>
2022-02-10 23:13:54 -0500

    Create LandingPage component and moved current App code to it

--- src/App.tsx
+++ src/App.tsx
@@ -1,28 +1,11 @@
-import React, { useState, useEffect } from "react";
-import Navbar from "./Components/Navbar/Navbar";
+import React from "react";
 import "./App.css";
+import LandingPage from "./Components/LandingPage";
 
 export default function App() {
-    const [width, setWidth] = useState(window.innerWidth);
-
-    const handleResize = () => {
-        setWidth(window.innerWidth);
-    }
-
-    useEffect(() => {
-        // Add listener for resizing
-        window.addEventListener("resize", handleResize);
-
-        // Clean up once unmounted
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
     return(
         <>
-            <div className="wrapper">
-                <div className="hero"><h1>Width: {width}</h1></div>
-                <Navbar />
-            </div>
+            <LandingPage />
         </>
     );
 }
---
+++ src/Components/LandingPage.tsx
@@ -0,0 +1,27 @@
+import React, { useState, useEffect } from "react";
+import "./Navbar";
+import Navbar from "./Navbar";
+import "./LandingPage.css";
+
+export default function LandingPage() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        // Add listener for resizing
+        window.addEventListener("resize", handleResize);
+
+        // Clean up once unmounted
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
+    return (
+        <div className="wrapper">
+            <div className="hero"><h1>Width: {width}</h1></div>
+            <Navbar />
+        </div>
+    )
+}
