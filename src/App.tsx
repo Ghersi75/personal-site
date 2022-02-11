211749b7e04185f9088fee18da10ffae63cb0a29
Ghersi Mac <34931324+Ghersi75@users.noreply.github.com>
2022-02-10 21:33:21 -0500

    Fixed background image resizing messing up and added temporary width state for responsive rendering

--- src/App.tsx
+++ src/App.tsx
@@ -1,12 +1,26 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import Navbar from "./Components/Navbar/Navbar";
 import "./App.css";
 
 export default function App() {
+    const [width, setWidth] = useState(window.innerWidth);
+
+    const handleResize = () => {
+        setWidth(window.innerWidth);
+    }
+
+    useEffect(() => {
+        // Add listener for resizing
+        window.addEventListener("resize", handleResize);
+
+        // Clean up once unmounted
+        return () => window.removeEventListener("resize", handleResize);
+    }, []);
+
     return(
         <>
             <div className="wrapper">
-                <div className="hero"> </div>
+                <div className="hero"><h1>Width: {width}</h1></div>
                 <Navbar />
             </div>
         </>
