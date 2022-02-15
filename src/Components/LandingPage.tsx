883678fc00845e8b4e6e5f6dfb343f155f58b979
Ghersi Mac <34931324+Ghersi75@users.noreply.github.com>
2022-02-14 21:46:38 -0500

    Added noreferrer noopener to landingpage links and removes handleresize that was added for testing

--- src/Components/LandingPage.tsx
+++ src/Components/LandingPage.tsx
@@ -1,4 +1,4 @@
-import React, { useState, useEffect } from "react";
+import React from "react";
 import "./Navbar";
 import "./LandingPage.css";
 import LinkedIn from "../iconComponents/LinkedIn";
@@ -9,21 +9,6 @@ interface Props {
 }
 
 export default function LandingPage(props: Props) {
-    const [width, setWidth] = useState(window.innerWidth);
-
-    const handleResize = () => {
-        setWidth(window.innerWidth);
-        console.log(width);
-    }
-
-    useEffect(() => {
-        // Add listener for resizing
-        window.addEventListener("resize", handleResize);
-
-        // Clean up once unmounted
-        return () => window.removeEventListener("resize", handleResize);
-    }, []);
-
     return (
         <div className="hero-wrapper" ref={props.ref}>
             <div className="hero">
@@ -31,8 +16,8 @@ export default function LandingPage(props: Props) {
                     <h1 className="hero-details">Ghersi Doci</h1>
                     <p className="hero-details">CSE student at UConn</p>
                     <div className="hero-details icons"> 
-                        <a href="https://github.com/Ghersi75" target="_blank" rel=""> <Github /> </a>
-                        <a href="https://www.linkedin.com/in/ghersi-doci-695839211/" target="_blank" rel=""> <LinkedIn /> </a>
+                        <a href="https://github.com/Ghersi75" target="_blank" rel="noreferrer noopener"> <Github /> </a>
+                        <a href="https://www.linkedin.com/in/ghersi-doci-695839211/" target="_blank" rel="noreferrer noopener"> <LinkedIn /> </a>
                     </div>
                 </div>
             </div>
