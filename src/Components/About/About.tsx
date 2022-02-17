4ea88a76ba627ebbc47aab80807c5e61a0eca14a
Ghersi Mac <34931324+Ghersi75@users.noreply.github.com>
2022-02-16 20:35:01 -0500

    Made about page responsive to screen size

--- src/Components/About/About.tsx
+++ src/Components/About/About.tsx
@@ -9,13 +9,25 @@ export default function AboutMe(props: Props) {
     return (
         <div className="about-wrapper" ref={props.ref}>
             <div className="about-text-wrapper left">
-                <div className="about-text">
-                    1
+                <div className="about-text left">
+                    <h1 className="about-h1 left">About</h1>
+                    <h2 className="about-h2 left">About myself</h2>
+                    <p className="about-p left">My name is Ghersi, and I’m currently pursuing a Computer Science and Engineering major at the University of Connecticut with a focus on software design and engineering.</p>
+                    <h2 className="about-h2 left">About experience</h2>
+                    <p className="about-p left">Outside of school I enjoy learning new things on my own. The latest thing I’ve spent time learning is frontend web development with React. Another thing I’ve worked on is blockchain development, mainly the Ethereum blockchain and any Ethereum forks such as BSC. For blockchain development I used a variety of tools and languages such as Solidity to write smart contracts and Typescript for contract testing.</p>
                 </div>
             </div>
             <div className="about-text-wrapper right">
-                <div className="about-text">
-                    2
+                <div className="about-text right">
+                    <h1 className="about-h1 right">Skills</h1>
+                    <h2 className="about-h2 right">Typescript:</h2>
+                    <p className="about-p right">Used Typescript for web development, solidity smart contract testing and some miscellaneous api based scripts.</p>
+                    <h2 className="about-h2 right">Python:</h2>
+                    <p className="about-p right">Used python for a variety of scripts and classwork, mainly data structures and algorithms and time complexity classwork.</p>
+                    <h2 className="about-h2 right">Solidity:</h2>
+                    <p className="about-p right">Wrote and deployed various ERC721 contracts and wrote ERC20 contracts.</p>
+                    <h2 className="about-h2 right">Other skills:</h2>
+                    <p className="about-p right">HTML, CSS, Git, Linux, Terminal, Mac.</p>
                 </div>
             </div>
         </div>
